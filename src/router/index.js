242c1ef5b81edc9f2a950bb8c799b0514abb21be
morvern-web <jeremy.harris@M-JHarris.local>
2024-05-12 21:46:00 +0200

    switch router history type

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,8 @@
-import { createRouter, createMemoryHistory } from 'vue-router';
+import { createRouter, createWebHashHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
 
 const router = createRouter({
-  history: createMemoryHistory(),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
